78f55e7799420df20e5b5d6b4ac551769e216fa4
Syed Atif <syedatif@thoughtworks.com>
2015-10-12 23:32:09 +0530

    Added query params

--- app/com/thoughtworks/vodqa/Controller/VodQAController.scala
+++ app/com/thoughtworks/vodqa/Controller/VodQAController.scala
@@ -1,13 +1,6 @@
 package com.thoughtworks.vodqa.Controller
 
-import com.thoughtworks.vodqa.Controller.User
 import play.api.libs.json._
-import play.api.cache.Cache
-import scala.concurrent.duration._
-import collection.mutable.Seq
-
-import scala.collection.immutable.HashSet
-
 
 case class User (val userId: Option[Int],val userName: String,val employer: String,val location: String)
 
@@ -28,9 +21,17 @@ object VodQAController extends Controller{
   var count: Int = 5
 
 
-
-  def listUsers() = Action{
-    request=>Ok(Json.toJson(listOfUsers.values)).withHeaders("Content-Type"->"application/json")
+  def listUsers(employer: Option[String],location: Option[String]) = Action{
+    request=>
+      val filteredUsers = listOfUsers.values.filter { user =>
+        employer
+          .map(_.equals(user.employer))
+          .getOrElse(true) &&
+          location
+            .map(_.equals(user.location))
+            .getOrElse(true)
+      }
+      Ok(Json.toJson(filteredUsers)).withHeaders("Content-Type"->"application/json")
   }
 
   def getUser(userId: Int) = Action{
@@ -51,7 +52,7 @@ object VodQAController extends Controller{
     request=>request.body.validate[User].map {
       case user =>
         count = count + 1
-        val newUser = user.copy(userId = Some(count))
+        val newUser = user copy(userId = Some(count))
         listOfUsers += (count -> newUser)
         Ok(Json.toJson(newUser)).withHeaders("Content-Type"->"application/json")
     }.recoverTotal(e=>BadRequest(s"$e"))
